fbd67bfbe1482bd6870701d16b2b369134b30d9e
GromGT12 <m.krymov@wp.pl>
2023-10-13 20:37:29 +0200

    I've added the logic in the remaining classes

--- src/main/java/com/example/bank/controller/BalanceController.java
+++ src/main/java/com/example/bank/controller/BalanceController.java
@@ -4,10 +4,12 @@ package com.example.bank.controller;
 import com.example.bank.model.TransferBalance;
 import com.example.bank.service.BankService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @RestController("/balance")
 @AllArgsConstructor
 public class BalanceController {
@@ -24,11 +26,18 @@ public class BalanceController {
     public BigDecimal addMoney(@RequestBody TransferBalance transferBalance) {
         return bankService.addMoney(transferBalance.getTo(), transferBalance.getAmount());
 
+
     }
 
     @PostMapping("/{transfer}")
     public void transfer(@RequestBody TransferBalance transferBalance) {
         bankService.makeTransfer(transferBalance);
 
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handle(IllegalArgumentException e) {
+        log.error(e.getMessage());
+        return "the program is broken";
     }
 }
--- src/main/java/com/example/bank/model/TransferBalance.java
+++ src/main/java/com/example/bank/model/TransferBalance.java
@@ -3,11 +3,13 @@ package com.example.bank.model;
 import lombok.Data;
 
 import java.math.BigDecimal;
-@Data
-public class TransferBalance{
+
+@Data//содержит геттеры, сеттеры, и toString и hashCode equals
+public class TransferBalance {
     private Long from;
     private Long to;
     private BigDecimal amount;
 
-
 }
+
+
